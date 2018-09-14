cb4571d3f2c086cc12168a30a980afc35e476dfa
Zhao Yonggang <yonggang.zhao@icloud.com>
2018-09-14 16:56:08 +0800

    weeknum from '2018/1/1 added'

---
+++ index.js
@@ -0,0 +1,20 @@
+Date.prototype.getWeek = function (dowOffset) {
+  /*getWeek() was developed by Nick Baicoianu at MeanFreePath: http://www.meanfreepath.com */
+  
+      dowOffset = typeof(dowOffset) == 'int' ? dowOffset : 0; //default dowOffset to zero
+      var newYear = new Date(2018,0,1);
+      var day = newYear.getDay() - dowOffset; //the day of week the year begins on
+      day = (day >= 0 ? day : day + 7);
+      var daynum = Math.floor((this.getTime() - newYear.getTime() - 
+      (this.getTimezoneOffset()-newYear.getTimezoneOffset())*60000)/86400000) + 1;
+      var weeknum;
+      //if the year starts before the middle of a week
+      if(day < 4) {
+          weeknum = Math.floor((daynum+day-1)/7) + 1;
+      }
+      else {
+          weeknum = Math.floor((daynum+day-1)/7);
+      }
+    
+      return this.getDay() === 0 ? (weeknum - 1) : weeknum;
+  };
